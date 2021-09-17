48d3c8d7a2bea6a4833b5b5fbe466822e70e3f0d
Gloria Edem <gloedem@gmail.com>
2021-09-17 02:48:11 +0000

    update create method, use flash to add notices on login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to :root
+      redirect_to '/'
     else
-      redirect_to '/login'
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    flash[:notice] = "You have successfully logged out"
     redirect_to '/login'
   end
 end
